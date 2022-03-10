b700e30679e68553439da8e43674a5620fcd696c
Neobef <fabianmaldonadosaba@gmail.com>
2022-03-10 12:04:27 -0300

    Viajes listo

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $("a").click(function () {
+    var gato = this.hash
+
+    $("html,body").animate({
+      scrollTop: $(gato).offset().top - 70
+    },
+      800
+    )
+  })
+})
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Viajes Chile
+Para este proyecto se utilizaron las siguientes herramientas
+Bootstrap
+Fontawesome
+GoogleFonts
